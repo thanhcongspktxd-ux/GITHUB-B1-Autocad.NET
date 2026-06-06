13fa09c79a571f64dd579e41790b40b792cd5ac3
Admin <Admin@ADMIN-PC>
2026-06-06 14:16:27 +0700

    Add project files.

---
+++ GITHUB B1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Internal;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Runtime.ConstrainedExecution;
+
+
+namespace GITHUB_B1
+{
+    public class Class1
+    {
+    }
+}
